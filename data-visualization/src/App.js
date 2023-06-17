e5f0b58513386b064fd37b1877babc3f8d967820
Prabhjeet <prabhjeetkalsi00@gmail.com>
2023-06-17 20:23:23 +0530

    [feat]: add bootstrap support

--- data-visualization/src/App.js
+++ data-visualization/src/App.js
@@ -2,8 +2,8 @@ import Graph from "./Graph.js";
 
 function App() {
   return (
-    <div className="App">
-      <h1>HEY!!</h1>
+    <div className="App h1">
+      HEY!!
       <Graph />
     </div>
   );
