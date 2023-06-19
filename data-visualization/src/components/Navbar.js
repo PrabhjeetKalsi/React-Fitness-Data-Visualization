e9d429e63484ebdfce6a23d393ea03d526e21171
Prabhjeet <prabhjeetkalsi00@gmail.com>
2023-06-19 13:33:40 +0530

    [feat]: add styling for navbar

--- data-visualization/src/components/Navbar.js
+++ data-visualization/src/components/Navbar.js
@@ -3,7 +3,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-custom">
       <div className="container-fluid justify-content-center">
-        <span className="navbar-brand mb-1 h1">Fitness Tracker</span>
+        <span className="display-6">Fitness Tracker</span>
       </div>
     </nav>
   );
