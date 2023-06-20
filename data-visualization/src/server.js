fd267122c11a035694f4c1485eb4329b26a1f4b3
Prabhjeet <prabhjeetkalsi00@gmail.com>
2023-06-20 17:42:58 +0530

    [feat]: connect server to MongoDB

--- data-visualization/src/server.js
+++ data-visualization/src/server.js
@@ -1,9 +1,23 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
 const port = process.env.PORT || 5000;
-const bodyParser = require("body-parser");
+const uri =
+  "mongodb+srv://prabhjeetkalsi:kalsi@fitnesstracker.ieduv0e.mongodb.net/?retryWrites=true&w=majority";
 
+const bodyParser = require("body-parser");
 app.use(bodyParser.json());
+
+async function connect() {
+  try {
+    await mongoose.connect(uri);
+    console.log("Connected to MongoDB.");
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+connect();
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
